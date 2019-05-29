ce22b019e623c7f6d942e3a7e508c55486470d1c
Duy Hoang <>
2019-05-29 13:40:43 +0800

    update

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,18 +3,6 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'
     ),
-  'src/components/DetailsView/DetailsView.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-details-view" */ 'src/components/DetailsView/DetailsView.mdx'
-    ),
-  'src/components/DetailsView/SectionDetailsView.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-section-details-view" */ 'src/components/DetailsView/SectionDetailsView.mdx'
-    ),
-  'src/components/DetailsView/SingleDetailsView.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-single-details-view" */ 'src/components/DetailsView/SingleDetailsView.mdx'
-    ),
   'src/components/EditForm/EditView.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-view" */ 'src/components/EditForm/EditView.mdx'
@@ -27,16 +15,28 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-labels-title" */ 'src/components/Labels/Title.mdx'
     ),
+  'src/components/DetailsView/DetailsView.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-details-view" */ 'src/components/DetailsView/DetailsView.mdx'
+    ),
+  'src/components/DetailsView/SectionDetailsView.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-section-details-view" */ 'src/components/DetailsView/SectionDetailsView.mdx'
+    ),
+  'src/components/DetailsView/SingleDetailsView.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-single-details-view" */ 'src/components/DetailsView/SingleDetailsView.mdx'
+    ),
   'src/components/EditForm/EditField/DateTimeField.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-date-time-field" */ 'src/components/EditForm/EditField/DateTimeField.mdx'
     ),
-  'src/components/EditForm/EditField/TextField.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-text-field" */ 'src/components/EditForm/EditField/TextField.mdx'
-    ),
   'src/components/EditForm/EditField/SelectField.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-select-field" */ 'src/components/EditForm/EditField/SelectField.mdx'
+    ),
+  'src/components/EditForm/EditField/TextField.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-text-field" */ 'src/components/EditForm/EditField/TextField.mdx'
     ),
 }
