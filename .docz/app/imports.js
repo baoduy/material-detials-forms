1975c8df63833cdebad9027029d04f78702d3592
Hoang, Duy <Duy.Hoang@Schroders.com>
2019-07-27 13:33:43 +0800

    update

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,9 +3,13 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'
     ),
-  'src/components/Core/ErrorMessage.mdx': () =>
+  'src/components/Labels/Label.mdx': () =>
     import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-core-error-message" */ 'src/components/Core/ErrorMessage.mdx'
+      /* webpackPrefetch: true, webpackChunkName: "src-components-labels-label" */ 'src/components/Labels/Label.mdx'
+    ),
+  'src/components/Labels/Title.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-labels-title" */ 'src/components/Labels/Title.mdx'
     ),
   'src/components/DetailsView/DetailsView.mdx': () =>
     import(
@@ -19,28 +23,24 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-details-view-single-details-view" */ 'src/components/DetailsView/SingleDetailsView.mdx'
     ),
+  'src/components/Core/ErrorMessage.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-core-error-message" */ 'src/components/Core/ErrorMessage.mdx'
+    ),
   'src/components/EditForm/EditView.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-view" */ 'src/components/EditForm/EditView.mdx'
-    ),
-  'src/components/Labels/Label.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-labels-label" */ 'src/components/Labels/Label.mdx'
-    ),
-  'src/components/Labels/Title.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-labels-title" */ 'src/components/Labels/Title.mdx'
     ),
   'src/components/EditForm/EditField/DateTimeField.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-date-time-field" */ 'src/components/EditForm/EditField/DateTimeField.mdx'
     ),
-  'src/components/EditForm/EditField/SelectField.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-select-field" */ 'src/components/EditForm/EditField/SelectField.mdx'
-    ),
   'src/components/EditForm/EditField/TextField.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-text-field" */ 'src/components/EditForm/EditField/TextField.mdx'
+    ),
+  'src/components/EditForm/EditField/SelectField.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-edit-form-edit-field-select-field" */ 'src/components/EditForm/EditField/SelectField.mdx'
     ),
 }
--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ import { css } from 'docz-plugin-css';
 
 export default {
   //src: ['./src', './node_modules/@material-ui'],
-  theme: 'docz-theme-ztopia',
+  //theme: 'docz-theme-ztopia',
   title: 'Material-UI Form Generation',
   wrapper: '.docz/wrapper/index.tsx',
   codeSandbox: false,
